3d79c215db7d3341eb7c9f208583c14299959198
yu-tcy <77433484+yu-tcy@users.noreply.github.com>
2021-10-26 01:05:23 +0900

    課題提出Check2
    Check
    Pet
    Constants

---
+++ JavaStudy/Check2/src/check/Check.java
@@ -0,0 +1,33 @@
+package check;
+
+import constants.Constants;
+
+public class Check {
+	
+	private String firstName;
+	private String lastName;
+	
+	public Check() {
+		this.firstName = "tsuchiya";
+		this.lastName = "yusuke";
+	}
+	
+	private void printName(String firstName, String lastName) {
+		System.out.println("printNameメソッド → " + firstName + lastName);
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		Check check = new Check();
+		check.printName(check.firstName, check.lastName);
+		
+		Pet pet = new Pet(Constants.CHECK_CLASS_JAVA, Constants.CHECK_CLASS_HOGE);
+		pet.introduce();
+		
+		RobotPet robotPet = new RobotPet(Constants.CHECK_CLASS_R2D2, Constants.CHECK_CLASS_LUKE);
+		robotPet.introduce();
+		}
+
+}
